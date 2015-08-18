4b2a7fa004efa777d95b33568f3feefd00e5ef9f
zor3ba <zor3ba@gmail.com>
2015-08-19 00:03:56 +0900

    first commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'view-cookbook'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures view-cookbook'
+long_description 'Installs/Configures view-cookbook'
+
+version          '0.1.0'
+
